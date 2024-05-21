165782a2d80ae4958ee07d5785da3a8632b393f7
Tahesha <taheshao@gmail.com>
2024-05-21 13:16:54 -0400

    Added the rest of Part 1.

--- JS/index.js
+++ JS/index.js
@@ -20,3 +20,63 @@ function parseCSV(csvString) {
 
 // Call the parseCSV function with the given CSV string
 parseCSV(csvString);
+
+
+
+//FizzBuzz
+// Loop through all numbers from 1 to 100
+for (let i = 1; i <= 100; i++) {
+    // Check if the number is divisible by both 3 and 5
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("Fizz Buzz");
+    } 
+    // Check if the number is divisible by 3
+    else if (i % 3 === 0) {
+        console.log("Fizz");
+    } 
+    // Check if the number is divisible by 5
+    else if (i % 5 === 0) {
+        console.log("Buzz");
+    } 
+    // If the number is not divisible by either 3 or 5
+    else {
+        console.log(i);
+    }
+}
+
+
+//PrimeTime
+// Function to check if a number is prime
+function isPrime(num) {
+    if (num <= 1) {
+        return false;
+    }
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Function to find the next prime number starting from n
+function findNextPrime(n) {
+    while (true) {
+        if (isPrime(n)) {
+            return n;
+        }
+        n++;
+    }
+}
+
+// Declare an arbitrary number, n
+const n = 4;
+
+// Find the next prime number starting from n
+const nextPrime = findNextPrime(n);
+
+// Log the next prime number
+console.log(`The next prime number after ${n} is: ${nextPrime}`);
+
+
+
